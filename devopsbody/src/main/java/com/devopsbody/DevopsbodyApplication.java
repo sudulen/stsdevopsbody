e391fbf05d1d393afdeb850623e241751c73e14b
a.hramovich <a.hramovich@a1qa.com>
2019-08-22 23:34:42 +0300

    initial

---
+++ devopsbody/src/main/java/com/devopsbody/DevopsbodyApplication.java
@@ -0,0 +1,13 @@
+package com.devopsbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsbodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsbodyApplication.class, args);
+	}
+
+}
---
+++ devopsbody/src/test/java/com/devopsbody/DevopsbodyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devopsbody;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevopsbodyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
